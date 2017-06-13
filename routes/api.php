ab7bf955152af1bfd3aa3ce451f8d914dd4eed84
zou_gj <751608899@qq.com>
2017-06-13 20:50:55 +0800

    dingoapi and jwt-auth config

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+    $api->group(['namespace'=>'App\Api\Controllers'],function($api){
+        $api->get('lessons','LessonsController@index');
+    });
 });
